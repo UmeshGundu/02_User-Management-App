1a4a8f55f4341a2268872872c85f0a6f3b3f3d82
Umesh <umeshgundu0610@gmail.com>
2025-03-20 10:05:19 +0530

    intial commit

---
+++ src/main/java/in/ashokit/dto/QuoteResponseDTO.java
@@ -0,0 +1,11 @@
+package in.ashokit.dto;
+
+import lombok.Data;
+
+@Data
+public class QuoteResponseDTO {
+	
+	private Integer id;
+	private String quote;
+	private String author;
+}
---
+++ src/main/java/in/ashokit/dto/ResetPwdDTO.java
@@ -0,0 +1,16 @@
+package in.ashokit.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ResetPwdDTO {
+	
+	private String email;
+	private String oldPwd;
+	private String newPwd;
+	private String confirmPwd;
+	
+
+}
